5a03c2d7e11fe4ad238903fdbc6a5a81bab70964
Markus Høgne <markushogne@gmail.com>
2024-10-27 18:09:57 +0700

    background fix

--- src/static/App.tsx
+++ src/static/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../shared/theme';
@@ -6,19 +6,20 @@ import { Header, Footer } from '../shared/components';
 import { LandingPage, AboutPage } from './components';
 
 const AppContent: React.FC = () => {
-    return (
-      <div className="app">
-        <Header />
-        <main className="main-content">  {/* Changed from div to main */}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    );
-  };
+
+  return (
+    <div className="app">
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (

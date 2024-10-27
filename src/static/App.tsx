913bcc5427aa6de32f70fc0ec4073e2af18766cb
Markus Høgne <markushogne@gmail.com>
2024-10-28 03:13:21 +0700

    index

--- src/static/App.tsx
+++ src/static/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../shared/theme';
@@ -7,22 +7,6 @@ import { LandingPage, AboutPage, ConfiguratorTemplate } from './components';
 import ErrorBoundary from '../shared/components/ErrorBoundary/ErrorBoundary';
 
 const AppContent: React.FC = () => {
-  useEffect(() => {
-    // Debug image loading
-    const img = new Image();
-    img.src = 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730055768/background_final_last_dm9bl2.pn';
-    img.onload = () => console.log('Background loaded');
-    img.onerror = (e) => console.error('Background failed to load:', e);
-  }, []);
-
-  useEffect(() => {
-    console.log('Static App: Background should be loaded from cache');
-    const img = new Image();
-    img.src = 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730055768/background_final_last_dm9bl2.png';
-    img.onload = () => console.log('Static App: Background loaded');
-    img.onerror = (e) => console.error('Static App: Background failed:', e);
-  }, []);
-
   return (
     <div className="app">
       <Header />

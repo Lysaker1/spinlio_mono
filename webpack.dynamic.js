6b445f00ab2cfd4e9c7835ef620e3ab7e2da948e
Markus Høgne <markushogne@gmail.com>
2024-10-27 10:59:50 +0700

    proxy

--- webpack.dynamic.js
+++ webpack.dynamic.js
@@ -105,5 +105,24 @@ module.exports = (env) => {
       historyApiFallback: true,
       headers: cspHeaders
     },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `vendor.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      },
+    },
+    performance: {
+      hints: false, // Disable size warnings for now
+    }
   };
 };

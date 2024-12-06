94b1ad5f9cb9340a0c0b24fb2683896ffbc237e5
JIeCoRyI3 <jiecoryi3@gmail.com>
2024-12-06 12:46:56 +0200

    *fix images *delete label *fix ui slider

--- src/dynamic/components/ConfiguratorPage/components/ParameterPanel/components/ParameterTypes/ShapeGrid/ShapeIcons.tsx
+++ src/dynamic/components/ConfiguratorPage/components/ParameterPanel/components/ParameterTypes/ShapeGrid/ShapeIcons.tsx
@@ -1,44 +1,50 @@
 export const ShapeIcons = {
     Oval: (
-      <svg viewBox="0 0 24 24">
-        <ellipse cx="12" cy="12" rx="10" ry="6" fill="none" stroke="white" strokeWidth="2"/>
-      </svg>
+        <svg width="71" height="43" viewBox="0 0 71 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M70.5 21.5C70.5 27.1947 66.6864 32.4335 60.3433 36.2751C54.0094 40.1111 45.2272 42.5 35.5 42.5C25.7728 42.5 16.9906 40.1111 10.6567 36.2751C4.31361 32.4335 0.5 27.1947 0.5 21.5C0.5 15.8053 4.31361 10.5665 10.6567 6.72488C16.9906 2.88887 25.7728 0.5 35.5 0.5C45.2272 0.5 54.0094 2.88887 60.3433 6.72488C66.6864 10.5665 70.5 15.8053 70.5 21.5Z"
+                stroke="white"/>
+        </svg>
     ),
     Capsule: (
-      <svg viewBox="0 0 24 24">
-        <rect x="4" y="8" width="16" height="8" rx="4" fill="none" stroke="white" strokeWidth="2"/>
-      </svg>
+        <svg width="69" height="46" viewBox="0 0 69 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.5" y="0.5" width="68" height="45" rx="22.5" stroke="white"/>
+        </svg>
     ),
     Hexagon: (
-      <svg viewBox="0 0 24 24">
-        <path d="M12 4L20 8V16L12 20L4 16V8L12 4Z" fill="none" stroke="white" strokeWidth="2"/>
-      </svg>
+        <svg width="56" height="52" viewBox="0 0 56 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M36.942 0.5C40.336 0.5 43.4722 2.31069 45.1692 5.25L54.1499 20.805C55.8469 23.7443 55.8469 27.3657 54.1499 30.305L45.1692 45.8599C43.4722 48.7992 40.336 50.6099 36.942 50.6099H18.9807C15.5866 50.6099 12.4504 48.7992 10.7534 45.8599L1.77276 30.305C0.0757485 27.3657 0.0757459 23.7443 1.77276 20.805L10.7534 5.25C12.4504 2.31069 15.5866 0.5 18.9807 0.5H36.942Z"
+                stroke="white"/>
+        </svg>
     ),
     Circle: (
-      <svg viewBox="0 0 24 24">
-        <circle cx="12" cy="12" r="8" fill="none" stroke="white" strokeWidth="2"/>
-      </svg>
+        <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="28" cy="28" r="27.5" stroke="white"/>
+        </svg>
     ),
     Triangle: (
-      <svg viewBox="0 0 24 24">
-        <path d="M12 4L20 18H4L12 4Z" fill="none" stroke="white" strokeWidth="2"/>
-      </svg>
+        <svg width="52" height="53" viewBox="0 0 52 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M1.02891 13.7439C0.68833 19.3142 3.44132 26.5091 4.86039 29.4103C6.92849 33.6384 10.395 40.2884 17.2065 47.2513C22.7572 52.9253 29.1267 52.4734 31.681 51.1679C34.2353 49.8624 40.1954 44.6402 46.5812 30.7145C52.967 16.7888 51.2642 9.3921 48.7098 6.78103C46.1555 4.16996 35.9382 0.253356 22.3151 1.12371C8.69208 1.99407 1.45463 6.78103 1.02891 13.7439Z"
+                stroke="white"/>
+        </svg>
     ),
     Square: (
-      <svg viewBox="0 0 24 24">
-        <rect x="6" y="6" width="12" height="12" fill="none" stroke="white" strokeWidth="2"/>
-      </svg>
+        <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.5" y="0.5" width="55" height="55" rx="14.5" stroke="white"/>
+        </svg>
     ),
     'Flat Mount': {
-      type: 'image',
-      src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730626307/Flatmount.png'
+        type: 'image',
+        src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730626307/Flatmount.png'
     },
     'Post Mount': {
-      type: 'image',
-      src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730626307/Postmount.png'
+        type: 'image',
+        src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730626307/Postmount.png'
     },
     'Quick Release': {
-      type: 'image',
+        type: 'image',
       src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/v1730626307/quickRelease.png'
     },
     'Bolt Through': {
@@ -47,14 +53,14 @@ export const ShapeIcons = {
     },
     'Matte': {
       type: 'image',
-      src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/matt_ic7bdc.png'
+      src: 'images/matt.png'
     },
     'Glossy': {
       type: 'image',
-      src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/glossy_wslz1x.png'
+      src: 'images/glossy.png'
     },
     'Metallic': {
       type: 'image',
-      src: 'https://res.cloudinary.com/da8qnqmmh/image/upload/metallic_o5umwt.png'
+      src: 'images/metallic.png'
     }
   };

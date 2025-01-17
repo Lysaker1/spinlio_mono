c9b4bcd1e7a5aeb233b0c34c016e206e6ca7284d
Markus Høgne <markushogne@gmail.com>
2025-01-17 13:20:52 +0100

    critical server fix

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -454,7 +454,7 @@ app.use((err: any, req: Request, res: Response, next: any) => {
   });
 });
 
-const PORT = process.env.PORT_API || 3003;
+const PORT = process.env.PORT|| 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

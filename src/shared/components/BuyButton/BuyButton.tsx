e0d7e86de6b5c96d97ed9e641043c82a07abbe15
Markus Høgne <markushogne@gmail.com>
2025-01-28 19:22:03 +0300

    add new payment links

--- src/shared/components/BuyButton/BuyButton.tsx
+++ src/shared/components/BuyButton/BuyButton.tsx
@@ -7,9 +7,15 @@ interface BuyButtonProps {
 }
 
 export const BuyButton: React.FC<BuyButtonProps> = ({ configuratorType }) => {
-  const paymentLink = configuratorType === CONFIGURATOR_TYPES.VULZ 
-    ? 'https://buy.stripe.com/test_3csbK516ch0Fe5y144'  // Vulz payment link
-    : 'https://buy.stripe.com/test_9AQ3dz7uAeSxbXqeUV';  // 
+  const PAYMENT_LINKS: Partial<Record<ConfiguratorType, string>> = {
+    [CONFIGURATOR_TYPES.VULZ]: 'https://buy.stripe.com/00g9AT08d3Hng2k9AA',
+    [CONFIGURATOR_TYPES.URBAN]: 'https://buy.stripe.com/9AQ14n4ot0vb2bu3cf',
+    [CONFIGURATOR_TYPES.STEPTHRU]: 'https://buy.stripe.com/00g5kD08d5PvcQ83ce',
+    [CONFIGURATOR_TYPES.DEFAULT]: 'https://buy.stripe.com/eVadR908d2DjaI0aEF'
+  };
+
+  const paymentLink = PAYMENT_LINKS[configuratorType] ?? PAYMENT_LINKS[CONFIGURATOR_TYPES.DEFAULT];
+
 
   const handleClick = () => {
     console.log('Buy button clicked, configuratorType:', configuratorType); // Debug log

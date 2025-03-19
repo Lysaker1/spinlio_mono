3f208afee381bdfb6d4d04fadedc5875d29383bc
Ola Hagerupsen Hansen <96570269+Ola-HH@users.noreply.github.com>
2025-03-19 17:37:32 +0000

    huge restructure

--- src/shared/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Header } from '../../dynamic/components/ConfiguratorPage/components/Header';
-export { default as Footer } from './Footer';
-export { default as ErrorBoundary } from './ErrorBoundary';

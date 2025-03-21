b1e4341660fc6db02d9e7a0563e6694fe29a1a6f
Markus Høgne <markushogne@gmail.com>
2025-03-21 13:38:37 +0800

    Add @emotion/styled dependency and alias to fix missing module error

--- src/shared/components/Auth/AuthenticatedFeature.tsx
+++ src/shared/components/Auth/AuthenticatedFeature.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import styled from '@emotion/styled';
+
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
